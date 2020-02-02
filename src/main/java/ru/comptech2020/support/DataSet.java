a33f5b8b4117c4f9becc8b546b44751bded41ae0
Alexander Trushev <haasix2@gmail.com>
2020-02-02 23:18:06 +0700

    add dataset generator

---
+++ src/main/java/ru/comptech2020/support/DataSet.java
@@ -0,0 +1,61 @@
+package ru.comptech2020.support;
+
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+
+/**
+ * gsutil -m cp -R gs://openmobiledata_public .
+ * files: 4906 of 4907, lines: 2080763
+ */
+public class DataSet {
+    public static void main(String[] args) throws IOException {
+        final String folder = "/Users/hsi/study/comptech2020/openmobiledata_public";
+        final String resultDataSet = "/Users/hsi/study/comptech2020/mobile_data.csv";
+        final FileOutputStream fileOutputStream = new FileOutputStream(resultDataSet);
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final List<Path> paths = Files.list(Paths.get(folder)).collect(Collectors.toList());
+        final int size = paths.size();
+        int fileCount = 0;
+        final long[] recordCount = {0};
+        for (Path path : paths) {
+            final ZipFile zipFile = new ZipFile(path.toString());
+            final Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                final ZipEntry zipEntry = entries.nextElement();
+                final InputStream inputStream = zipFile.getInputStream(zipEntry);
+                final JsonNode json = objectMapper.readTree(inputStream);
+                json.elements().forEachRemaining(event -> {
+                    final String ctn = event.get("id").asText();
+                    final String timestamp = event.get("timestamp").asText();
+                    final JsonNode location = event.get("device_properties").get("location");
+                    final String lat = location.get("latitude").asText();
+                    final String lon = location.get("longitude").asText();
+                    if (!lat.equals("0.0") || !lon.equals("0.0")) {
+                        final String record = String.join(",", ctn, lat, lon, timestamp) + "\n";
+                        try {
+                            fileOutputStream.write(record.getBytes());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        recordCount[0]++;
+                    }
+                });
+            }
+            System.out.println(String.format("files: %d of %d, lines: %d", fileCount++, size, recordCount[0]));
+        }
+        fileOutputStream.close();
+    }
+}
