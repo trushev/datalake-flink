36f85eadb8efe4e78c3abbe2f417f00614dc03ab
Alexander Trushev <haasix2@gmail.com>
2020-02-02 17:07:31 +0700

    update ParseEventTest

--- src/test/java/ru/comptech2020/middlewares/ParseEventTest.java
+++ src/test/java/ru/comptech2020/middlewares/ParseEventTest.java
@@ -1,18 +1,18 @@
 package ru.comptech2020.middlewares;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import ru.comptech2020.events.UserLocation;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class ParseEventTest {
+public class ParseEventTest {
     @Test
-    void map_UserLocationAsCsv_PresentOptionalOfUserLocation() {
-        final String input = "79993002010,54.84296,83.091047,1580632599";
+    public void map_UserLocationAsCsv_PresentOptionalOfUserLocation() {
+        final String input = "79993002010,54.84296,83.091047,1580632599000";
         final Optional<UserLocation> expected = Optional.of(new UserLocation(
-                "79993002010", 54.84296, 83.091047, "1970-01-19T07:03:52.599"
+                "79993002010", 54.84296, 83.091047, "2020-02-02T08:36:39"
         ));
         final ParseEvent parseEvent = new ParseEvent("user_location");
         assertEquals(expected, parseEvent.map(input));
